0e1f785302de316d9b6178639bc415db4dc57d07
RashidMall <mr.mallaev@gmail.com>
2018-06-18 00:46:24 +0200

    Add query to find if the the theater is empty -- Not working properly

--- request.sql
+++ request.sql
@@ -25,4 +25,9 @@ SELECT Client.id, client_nom, client_prenom, film_titre FROM Client
     INNER JOIN Film
     	ON Seance.idFilm = Film.id
 WHERE film_titre = "Avengers" AND debut_seance BETWEEN "18-06-11" AND "18-06-18";
+
 --    Comment savoir si une salle est pleine?
+SELECT * FROM Ticket
+	RIGHT JOIN Siege
+		ON Ticket.idSiege = Siege.id
+WHERE idSalle = 1 AND (idSiege IS NULL OR idSeance != 1);
