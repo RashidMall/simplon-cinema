04bab9a3418659c5fe56f8c5f6eac0aca017d605
Simplejul <38561418+Simplejul@users.noreply.github.com>
2018-06-17 20:19:35 +0200

    Create request.sql
    requetes en fonction des demandes.

---
+++ request.sql
@@ -0,0 +1,12 @@
+-- SELECT * FROM Salle
+
+--    Comment récupérer la liste des clients?
+-- SELECT client_nom, client_prenom FROM Client
+
+--    Comment récupérer la liste des films?
+-- SELECT film_titre FROM Film
+
+--    Comment récupérer la liste des clients qui ont vu un film donné?
+
+--    Comment récupérer la liste des clients qui ont vu un film cette semaine?
+--    Comment savoir si une salle est pleine?
